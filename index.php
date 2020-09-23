10be5a149f550c91f7f6f73665828faff91b0ea3
unknown <mau9977@gmail.com>
2020-09-22 23:27:07 -0500

    cambio a .php para deploy

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <link rel=stylesheet type="text/css" href="vendors/css/normalize.css">
+        <link rel=stylesheet type="text/css" href="vendors/css/grid.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;1,300&display=swap" rel="stylesheet">
+        <title>
+            Portafolio
+        </title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="NavFixedWrapper">
+                    <div class="Title">
+                        <p>
+                            ML
+                        </p>
+                    </div>
+                    <div class="HeaderNavRight">
+                        <a class="Button Button-full" href="*">
+                            Proyectos
+                        </a>
+                        <a class="Button Button-ghost" href="*">
+                            Acerca de mi
+                        </a>
+                        <a class="Button Button-ghost" href="*">
+                            Contactos
+                        </a>  
+                    </div>
+                </div>
+            </nav>
+            <div class="background-text-box">
+                <h1>
+                    ¡Hola! Mi nombre es Mauricio Linares, soy desarrollador web backend y de videojuegos en Unity.
+                </h1>
+            </div>
+        </header>
+          
+    
+    </body>
+</html>
